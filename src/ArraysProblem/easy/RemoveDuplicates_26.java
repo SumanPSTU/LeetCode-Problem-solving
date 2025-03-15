1d64b7dcf00bb05054080705ce8a0fe8c05f1bd1
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-03-15 22:04:10 +0600

    update

--- src/ArraysProblem/easy/RemoveDuplicates_26.java
+++ src/ArraysProblem/easy/RemoveDuplicates_26.java
@@ -6,7 +6,11 @@ public class RemoveDuplicates_26 {
     public int removeDuplicates(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int i:nums){
-            if ()
+            set.add(i);
         }
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 0;
     }
 }
