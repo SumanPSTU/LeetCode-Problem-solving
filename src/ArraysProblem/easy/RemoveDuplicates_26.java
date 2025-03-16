f167bc3aa6b060c1e2fb1b12a2a67ed5d2e81fd0
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-03-16 20:44:46 +0600

    new problem solve

--- src/ArraysProblem/easy/RemoveDuplicates_26.java
+++ src/ArraysProblem/easy/RemoveDuplicates_26.java
@@ -4,13 +4,19 @@ import java.util.HashSet;
 
 public class RemoveDuplicates_26 {
     public int removeDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i:nums){
-            set.add(i);
-        }
-        for (int i = 0; i < nums.length; i++) {
+       int count = 0;
+       for (int i =0;i<nums.length;i++){
+           if ((i<nums.length-1) && (nums[i] == nums[i+1])){
+               continue;
+           }else {
+               nums[count++] = nums[i];
+           }
+       }
+       return count;
+    }
 
-        }
-        return 0;
+    public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        System.out.println(new RemoveDuplicates_26().removeDuplicates(nums));
     }
 }
