74e46630ef65d3bcab4ba1db4831fcf7315aa55f
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-05-03 20:43:14 +0600

    add two sum problem

--- src/Math/Easy/TwoSum_1.java
+++ src/Math/Easy/TwoSum_1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class TwoSum_1 {
     public int[] twoSum(int[] nums, int target) {
        for (int i =0;i<nums.length;i++){
-           for (int j = 0; j < nums.length; j++) {
+           for (int j = i+1; j < nums.length; j++) {
                if (nums[i]+nums[j]==target){
                    return new int[]{i,j};
                }
@@ -14,9 +14,9 @@ public class TwoSum_1 {
        return null;
     }
     public static void main(String[] args){
-        int[] array = {1,2,3,4,5,6};
-        int target = 9;
         var two = new TwoSum_1();
+        int[] array = {1,2,3,4,5,6,7};
+        int target = 11;
         System.out.println(Arrays.toString(two.twoSum(array,target)));
     }
 }
