a8525522ffd9124c716affe6b22a827f231040a7
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-05-09 23:15:15 +0600

    solve Product of array problem

--- src/ArraysProblem/ProductOfArrayExpectSelf_238.java
+++ src/ArraysProblem/ProductOfArrayExpectSelf_238.java
@@ -23,11 +23,32 @@ public class ProductOfArrayExpectSelf_238 {
         return ans;
 
     }
+
+    public static int[] productExceptSelfOther(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+
+        // Prefix product (left side product)
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // Suffix product (right side product)
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= suffix;  // Multiply with suffix
+            suffix *= nums[i];  // Update suffix product
+        }
+
+        return ans;
+    }
     public static void main(String[] args){
         int[] array = {1,2,3,4};
 
         int[] arr = productExceptSelf(array);
         System.out.println(Arrays.toString(arr));
+        int[] ar = productExceptSelfOther(array);
+        System.out.println(Arrays.toString(ar));
 
     }
 }
