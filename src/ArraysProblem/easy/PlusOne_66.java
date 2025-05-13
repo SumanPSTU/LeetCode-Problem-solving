04dd6e234ca82ce50a0e02eb6be5eba3277c94fd
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-05-13 21:59:17 +0600

    solve one plus problem

--- src/ArraysProblem/easy/PlusOne_66.java
+++ src/ArraysProblem/easy/PlusOne_66.java
@@ -1,36 +1,33 @@
 package ArraysProblem.easy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Stack;
 
 public class PlusOne_66 {
     public static int[] plusOne(int[] digits) {
-        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int c = 0;
         int sum = 0;
-        for(int i:digits){
-            sum = sum*10+i;
+        for (int i : digits) {
+            sum = sum * 10 + i;
         }
-        sum=sum+1;
+        sum = sum + 1;
         while (sum>0){
-            int rem = sum%10;
-            list.add(rem);
-            sum/=10;
-
+            int rem = sum %10;
+            stack.push(rem);
+            sum /= 10;
+            c++;
         }
-        Collections.sort(list,Collections.reverseOrder());
-        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
-        for (int i =0;i<array.length;i++){
-            int temp = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = temp;
-
+        int[] array = new int[3];
+        for (int i = 0;i<array.length;i++){
+            array[i] = stack.pop();
         }
+
         return array;
-
     }
-    public static void main(String[] args){
-        int[] array = {1,2,3};
+
+    public static void main(String[] args) {
+        int[] array = {9};
         System.out.println(Arrays.toString(plusOne(array)));
     }
 }
